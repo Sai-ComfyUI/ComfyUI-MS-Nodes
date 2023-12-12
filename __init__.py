55e16ae8a750196483707fc9631fc3e2735554ac
sai.ling_凌倫明 <saizycaopera@outlook.com>
2023-12-12 15:14:50 +0800

    fix module path search

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import os
 
 root = Path(__file__).parent.resolve()
 py_folder = Path('%s/py' % root)
+custom_nodes_path = Path(root).parents[1]
 
 py_files = list(py_folder.rglob("*.py"))
 
@@ -14,7 +15,7 @@ NODE_CLASS_MAPPINGS = dict()
 NODE_DISPLAY_NAME_MAPPINGS = dict()
 
 for file in ui_files:
-    relpath = (os.path.relpath(file, os.getcwd()))
+    relpath = (os.path.relpath(file, custom_nodes_path))
     module_path = '.'.join(relpath.split('.')[0].split('\\'))
     ui_module = importlib.import_module(module_path)
     NODE_CLASS_MAPPINGS.update(ui_module.NODE_CLASS_MAPPINGS)
