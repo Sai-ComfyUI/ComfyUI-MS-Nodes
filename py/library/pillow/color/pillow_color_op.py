bd51f8dd2e5daa278986085dc0214dc07f3989a6
sai.ling_凌倫明 <saizycaopera@outlook.com>
2024-02-01 10:08:52 +0800

    add apply luts form cube and hald file

--- py/library/pillow/color/pillow_color_op.py
+++ py/library/pillow/color/pillow_color_op.py
@@ -1,10 +1,29 @@
-from pillow_lut import load_cube_file 
-from .....modules import image_funcs
+from PIL import Image
+import io
+from pillow_lut import load_cube_file, load_hald_image, rgb_color_enhance
+from .....modules import image_funcs, folder_paths
 
-def pillow_apply_luts(image, luts_file):
+luts_folder = r"%s\luts" % folder_paths.folder_names_and_paths['assets']
+
+def pillow_apply_cube_luts(image, luts_file):
     image = image_funcs.tensor_to_pil(image)
+    luts_file = r"%s\%s" % (luts_folder, luts_file)
     color_lut = load_cube_file(luts_file)
     image = image.filter(color_lut)
+    image = image_funcs.pil_to_tensor(image)
+
+    return image
+
+def pillow_hald_image(image, hald_image):
+    image = image_funcs.tensor_to_pil(image)
+    hald_image = image_funcs.tensor_to_pil(hald_image)
+    #force to png
+    png_data = io.BytesIO()
+    hald_image.save(png_data, format='PNG')
+    png_data.seek(0)
+
+    luts_from_image = load_hald_image(png_data)
+    image = image.filter(luts_from_image)
     image = image_funcs.pil_to_tensor(image)
 
     return image
--- py/library/pillow/color/pillow_color_ui.py
+++ py/library/pillow/color/pillow_color_ui.py
@@ -2,10 +2,10 @@ import os
 from .....modules import comm_funcs, folder_paths
 from . import pillow_color_op as op
 
-luts_folder = folder_paths.folder_names_and_paths['assets']
-luts_files = comm_funcs.list_files_with_extensions(luts_folder, extensions=[".cube"])
+luts_folder = r"%s\luts" % folder_paths.folder_names_and_paths['assets']
+luts_files = comm_funcs.list_files_with_extensions(luts_folder, extensions=[".cube"], rel_path=True)
 
-class Pillow_LUTs:
+class Pillow_LUTs_FromCube:
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -22,16 +22,39 @@ class Pillow_LUTs:
     CATEGORY = comm_funcs.category_from_file(os.path.abspath(__file__))
 
     def run(self, image, luts_file):
-        image_tensor = op.pillow_apply_luts(image, luts_file)
+        image_tensor = op.pillow_apply_cube_luts(image, luts_file)
+        return (image_tensor,)
+
+
+class Pillow_LUTs_FromHaldImage:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required":
+                {
+                "image": ("IMAGE",),
+                "hald_image": ("IMAGE",),
+                },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "run"
+    CATEGORY = comm_funcs.category_from_file(os.path.abspath(__file__))
+
+    def run(self, image, hald_image):
+        image_tensor = op.pillow_hald_image(image, hald_image)
         return (image_tensor,)
 
 
 NODE_CLASS_MAPPINGS = {
-    "Pillow_LUTs": Pillow_LUTs,
+    "Pillow_LUTs_FromCube": Pillow_LUTs_FromCube,
+    "Pillow_LUTs_FromHaldImage": Pillow_LUTs_FromHaldImage,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Pillow_LUTs": "Pillow LUTs (MS)",
+    "Pillow_LUTs_FromCube": "Pillow Apply Cube LUTs (MS)",
+    "Pillow_LUTs_FromHaldImage": "Pillow Apply LUTs From Hald (MS)",
 }
 
