6e7bcd9b031688cd47bba0c6d16d03246844e127
sai.ling_凌倫明 <saizycaopera@outlook.com>
2024-02-12 14:24:14 +0800

    add pillow_rgb_color_enhance

--- py/library/pillow/color/pillow_color_op.py
+++ py/library/pillow/color/pillow_color_op.py
@@ -17,13 +17,23 @@ def pillow_apply_cube_luts(image, luts_file):
 def pillow_hald_image(image, hald_image):
     image = image_funcs.tensor_to_pil(image)
     hald_image = image_funcs.tensor_to_pil(hald_image)
-    #force to png
+    #force to 512*512 png
+    hald_image = hald_image.resize((512, 512))
     png_data = io.BytesIO()
     hald_image.save(png_data, format='PNG')
     png_data.seek(0)
 
     luts_from_image = load_hald_image(png_data)
     image = image.filter(luts_from_image)
+    image = image_funcs.pil_to_tensor(image)
+
+    return image
+
+
+def pillow_rgb_color_enhance(image, table_size, brightness, exposure, contrast, warmth, saturation, vibrance, hue, gamma, linear):
+    image = image_funcs.tensor_to_pil(image)
+    color_lut = rgb_color_enhance(table_size, brightness, exposure, contrast, warmth, saturation, vibrance, hue, gamma, linear)
+    image = image.filter(color_lut)
     image = image_funcs.pil_to_tensor(image)
 
     return image
--- py/library/pillow/color/pillow_color_ui.py
+++ py/library/pillow/color/pillow_color_ui.py
@@ -47,14 +47,47 @@ class Pillow_LUTs_FromHaldImage:
         return (image_tensor,)
 
 
+class Pillow_RGBColorEnhance:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required":
+                {
+                "image": ("IMAGE",),
+                "table_size": ("INT", {"default": 11, "min": 2, "max": 65, "step": 1}),
+                "brightness": ("FLOAT", {"default": 0, "min": -1, "max": 1, "step": 0.01}),
+                "exposure": ("FLOAT", {"default": 0, "min": -5, "max": 5, "step": 0.01}),
+                "contrast": ("FLOAT", {"default": 0, "min": -1, "max": 5, "step": 0.01}),
+                "warmth": ("FLOAT", {"default": 0, "min": -1, "max": 1, "step": 0.01}),
+                "saturation": ("FLOAT", {"default": 0, "min": -1, "max": 5, "step": 0.01}),
+                "vibrance": ("FLOAT", {"default": 0, "min": -1, "max": 5, "step": 0.01}),
+                "hue": ("FLOAT", {"default": 0, "min": 0, "max": 1, "step": 0.01}),
+                "gamma": ("FLOAT", {"default": 1, "min": 0, "max": 10, "step": 0.01}),
+                "linear": ("BOOLEAN", {"default": False}),
+                },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("output_image",)
+    FUNCTION = "run"
+    CATEGORY = comm_funcs.category_from_file(os.path.abspath(__file__))
+
+    def run(self, image, table_size, brightness, exposure, contrast, warmth, saturation, vibrance, hue, gamma, linear):
+        out = image
+        out = op.pillow_rgb_color_enhance(image, table_size, brightness, exposure, contrast, warmth, saturation, vibrance, hue, gamma, linear)
+        return (out,)
+
+
 NODE_CLASS_MAPPINGS = {
     "Pillow_LUTs_FromCube": Pillow_LUTs_FromCube,
     "Pillow_LUTs_FromHaldImage": Pillow_LUTs_FromHaldImage,
+    "Pillow_RGBColorEnhance": Pillow_RGBColorEnhance,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Pillow_LUTs_FromCube": "Pillow Apply Cube LUTs (MS)",
     "Pillow_LUTs_FromHaldImage": "Pillow Apply LUTs From Hald (MS)",
+    "Pillow_RGBColorEnhance": "Pillow RGB Color Enhance (MS)",
 }
 
